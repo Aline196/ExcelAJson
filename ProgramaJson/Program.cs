38e533dd39166e1fb5d2a01407d049b91cfacb6d
Maria Gonzalez <mariaeligiagonzalezuc@gmail.com>
2022-10-21 14:24:41 -0500

    Se agregae el metodo para convertir la lista de objetos en Json

--- ProgramaJson/Program.cs
+++ ProgramaJson/Program.cs
@@ -15,15 +15,42 @@ namespace ProgramaJson
     public class Program
     {
 
-        static void Main(String[] args)
+       
+        string rutaArchivo;
+        int tamanoFilas;
+        int tamanoColumnas;
+
+        
+
+        static void Main(string[] args)
         {
+            Program pg = new Program();
+            List<Ente> listaEnte = new List<Ente>();
 
-           var ente = GetEnte();
-
+            var ente = GetEnte();
+            SerializeJsonFile(listaEnte);
+            
         }
 
+        public static void SerializeJsonFile(List<Ente> listaEnte)
+        {
+            try
+            {
+                string rutaDestino = @"C:/prueba/Ente.json";
+                string generarJson = JsonConvert.SerializeObject(listaEnte.ToArray(), Formatting.Indented);
+                File.WriteAllText(rutaDestino, generarJson);
+            }
+            catch (Exception exp)
+            {
+
+                Console.WriteLine(exp.ToString());
+            }
+
+
+        }
         public static List<Ente> GetEnte()
         {
+            
             string rutaArchivo;
             Console.WriteLine("Ingrese la ruta del archivo");
             rutaArchivo = Console.ReadLine();
@@ -31,8 +58,8 @@ namespace ProgramaJson
             SLDocument sl = new SLDocument(rutaArchivo);
             SLWorksheetStatistics propiedades = sl.GetWorksheetStatistics();
 
-            int tamanoFila = propiedades.EndRowIndex; //10
-            int tamanoColumna = propiedades.EndColumnIndex; //6
+            int tamanoFila = propiedades.EndRowIndex; 
+            int tamanoColumna = propiedades.EndColumnIndex; 
 
 
             List<Ente> listaEnte = new List<Ente>();
@@ -40,59 +67,53 @@ namespace ProgramaJson
             //while (!string.IsNullOrEmpty())   
             for (int i = 2; i <= tamanoFila; i++)
             {
-                 var objEnte = new Ente();
-                
+                var objEnte = new Ente();
+
 
                 for (int j = 1; j < tamanoColumna;)
                 {
                     var nivelGobierno = new NivelGobierno();
+                    var datosDeControl = new DatosDeControl();
                     var entidadFederativa = new EntidadFederativa();
-        
+
                     objEnte.EnteDes = sl.GetCellValueAsString(i, j);
+
                     objEnte.NombreCorto = sl.GetCellValueAsString(i, ++j);
 
                     nivelGobierno.Nombre = sl.GetCellValueAsString(i, ++j);
 
-
                     objEnte.Poder = sl.GetCellValueAsString(i, ++j);
-
+                    objEnte.tipoEntidad = sl.GetCellValueAsString(i, ++j);
                     entidadFederativa.entidadFederativaDesc = sl.GetCellValueAsString(i, ++j);
-                    nivelGobierno.EntidadFederativa = entidadFederativa;
-                    objEnte.NivelGobierno = nivelGobierno;
-
                     objEnte.Ramo = sl.GetCellValueAsDouble(i, ++j);
                     objEnte.Rfc = sl.GetCellValueAsString(i, ++j);
 
-                   
+                    nivelGobierno.EntidadFederativa = entidadFederativa;
+                    objEnte.NivelGobierno = nivelGobierno;
+                    entidadFederativa.entidadFederativaDesc = "Campeche";
+                    objEnte.IdentificadorINAI = 1;
+                    objEnte.SecNac = "No SEC NAC";
+                    
+                    objEnte.unidadResponsable = "gUARDIA";
+                    objEnte.idEnteOrigen = "121345tty";
 
-
+                    datosDeControl.usuarioActualiza = 1;
+                    datosDeControl.situacion = "normal";
+                    //datosDeControl.fechaRegistro = "";
+                    //datosDeControl.fechaUltimaActualiza = '';
+                    datosDeControl.usuarioRegistra = 1;
+                    datosDeControl.activo = 1;
 
 
 
 
                 }
-                
+
                 listaEnte.Add(objEnte);
 
             }
 
             return listaEnte;
-        }
-
-        public static void serializarJson(List<Ente> ent)
-        {
-            //string obtenerJson = JsonConverter(ent.ToArray(), Formatting.Indented);
-
-            //JsonConverter.Ser
-
-
-
-
-
-
-
-
-
         }
         
 
